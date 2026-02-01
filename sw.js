932b9f2d103c5a84709db87b2382985e0b3c4b39
gefenrose <112893681+gefenrose@users.noreply.github.com>
2026-02-01 19:11:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'minute-journal-cache-v1';
+const urlsToCache = [
+'./',
+'./index.html',
+'./manifest.json',
+'./style.css',
+'./script.js',
+'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+
+self.addEventListener('install', event => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(urlsToCache))
+);
+});
+
+
+self.addEventListener('fetch', event => {
+event.respondWith(
+caches.match(event.request)
+.then(response => response || fetch(event.request))
+);
+});
+
+
+self.addEventListener('activate', event => {
+event.waitUntil(
+caches.keys().then(keys => Promise.all(
+keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+))
+);
+});
